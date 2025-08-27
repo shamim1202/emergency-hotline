a99da6df8e93a0dffbef1642db8413086b13c480
shamim1202 <mehedi.farzu@gmail.com>
2025-08-27 23:21:23 +0600

    Complete all card call btn functionality dynamically with added on show history section

---
+++ script.js
@@ -0,0 +1,33 @@
+const callBtns = document.getElementsByClassName("btn-call");
+for (const callBtn of callBtns) {
+  callBtn.addEventListener("click", (e) => {
+    const title =
+      callBtn.parentNode.parentNode.children[1].children[0].innerText;
+
+    const number =
+      callBtn.parentNode.parentNode.children[2].children[0].innerText;
+
+    const date = new Date().toLocaleTimeString();
+
+    const coin = document.getElementById("coin").innerText;
+    let currentCoin = Number(coin) - 20;
+
+    if (currentCoin < 0) {
+      return alert("Don't Have Sufficient Coin!");
+    } else {
+      const callHistoryDiv = document.getElementById("history");
+      const div = document.createElement("div");
+      div.innerHTML = `<div class="bg-[#f5fff6] flex items-center justify-between my-5 px-3 py-4 rounded-xl">
+            <div>
+              <p>${title}</p>
+              <p>${number}</p>
+            </div>
+            <div>
+              <p>${date}</p>
+            </div>
+          </div>`;
+      callHistoryDiv.appendChild(div);
+      document.getElementById("coin").innerText = currentCoin;
+    }
+  });
+}
